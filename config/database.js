76e06bb8474e6889f51548a5199fb940c2df33c2
Synaesthetic-Nemophilist <inflamer777@hotmail.com>
2016-06-23 18:34:29 +0300

    A clearer backend
    Added the code to the fresh file struct of the backend side (node)

---
+++ config/database.js
@@ -0,0 +1,3 @@
+module.exports = {
+       url : 'mongodb://SynNemo:synny@ds021034.mlab.com:21034/hungrynemo'
+   };
--- server.js
+++ server.js
@@ -8,7 +8,9 @@ var bodyParser = require('body-parser');  // all info from html POST (exp4)
 var methodOverride = require('method-override');  // simulate DELETE and PUT (exp4)
 
 //configuration=================================================================
-mongoose.connect('mongodb://SynNemo:synny@ds021034.mlab.com:21034/hungrynemo');  // conn to mongodb on mlab
+// load the config
+var database = require('./config/database');
+mongoose.connect(database.url);     // connect to mongoDB database on mlab
 
 app.use(express.static(__dirname + '/public'));  // serve static files in that dir
 app.use(morgan('dev'));
@@ -18,73 +20,8 @@ app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse applica
 app.use(methodOverride());
 
 
- //define model =================================================================
-var Todo = mongoose.model('Todo', {
-    text : String  // just a string, mongo automatically adds ids
-});
-
-
-// routes ======================================================================
-
-// THE API ---------------------------------------------------------------------
-// GET all todos
-app.get('/api/todos', function(req, res) { //GET method of api is available @ /api/todos route
-
-    // use mongoose to get all todos in the database
-    Todo.find(function(err, todos) {
-
-        // if there is an error retrieving, send the error. nothing after res.send(err) will execute
-        if (err)
-            res.send(err)
-
-        res.json(todos); // return all todos in JSON format
-    });
-});
-
-// POST todo and send back all todos after creation
-app.post('/api/todos', function(req, res) {
-
-    // create a todo, information comes from AJAX request from Angular
-    Todo.create({
-        text : req.body.text,
-        done : false
-    }, function(err, todo) {
-        if (err)
-            res.send(err);
-
-        // get and return all the todos after you create another
-        Todo.find(function(err, todos) {
-            if (err)
-                res.send(err)
-            res.json(todos);
-        });
-    });
-});
-
-// DELETE a todo
-app.delete('/api/todos/:todo_id', function(req, res) {
-    Todo.remove({
-        _id : req.params.todo_id
-    }, function(err, todo) {
-        if (err)
-            res.send(err);
-
-        // get and return all the todos after you del another
-        Todo.find(function(err, todos) {
-            if (err)
-                res.send(err)
-            res.json(todos);
-        });
-    });
-});
-
-
-// application -------------------------------------------------------------
-    app.get('*', function(req, res) {
-        res.sendfile('./public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
-    });
-
-
+// load the routes  ============================================================
+require('./app/routes')(app);
 
 
 // listen (start app with node server.js) ======================================
